13858b8ae1d984e560000f3e815364d3f130d8cb
yosue891 <yosueortega630@gmail.com>
2025-12-01 17:12:57 -0400

    Create group-verprimary.js

---
+++ plugins/group-verprimary.js
@@ -0,0 +1,18 @@
+// créditos reservados para wilkerson
+let handler = async (m, { conn }) => {
+  let chat = global.db.data.chats[m.chat]
+  if (!chat || !chat.primaryBot) {
+    return m.reply('ℹ️ No hay ningún bot principal establecido en este grupo.')
+  }
+
+  let botJid = chat.primaryBot
+  m.reply(`🤖 El bot principal en este grupo es:\n*${botJid}*`)
+}
+
+handler.help = ['verprimary']
+handler.tags = ['serbot']
+handler.command = ['verprimary']
+handler.group = true
+handler.admin = true
+
+export default handler
