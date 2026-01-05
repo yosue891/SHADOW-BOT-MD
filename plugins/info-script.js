0a92fb29c1a35b14efb3b3b3e8c22120f22b689e
yosue891 <yosueortega630@gmail.com>
2026-01-05 13:11:16 -0400

    Update info-script.js

--- plugins/info-script.js
+++ plugins/info-script.js
@@ -7,7 +7,7 @@ let handler = async (m, { conn }) => {
     if (!res.ok) throw new Error('Error al obtener datos del repositorio Shadow-BOT-MD')
     let json = await res.json()
 
-    let txt = `*🌑🎄  S H A D O W - B O T - M D  🎄🌑*\n\n`
+    let txt = `*🌑⚔️  S H A D O W - B O T - M D  ⚔️🌑*\n\n`
     txt += `⚔️  *Nombre del Proyecto* : ${json.name}\n`
     txt += `👀  *Observadores en las Sombras* : ${json.watchers_count}\n`
     txt += `📦  *Tamaño del Dominio* : ${(json.size / 1024).toFixed(2)} MB\n`
@@ -15,15 +15,16 @@ let handler = async (m, { conn }) => {
     txt += `🔗  *Portal Secreto* : ${json.html_url}\n`
     txt += `🌌  *Clones en las Sombras* : ${json.forks_count}\n`
     txt += `⭐  *Estrellas en la Oscuridad* : ${json.stargazers_count}\n\n`
-    txt += `❄️🎁 *"En las sombras, incluso en Navidad, el poder se oculta tras la calma..."* 🎁❄️\n\n`
+    txt += `『☽』 *En las sombras, el poder se oculta tras la calma...*\n\n`
     txt += `👑  *Creador*: Yosue`
 
     await conn.sendMessage(m.chat, {
       text: txt,
+      ...rcanal,
       contextInfo: {
         externalAdReply: {
           title: "Shadow-BOT-MD",
-          body: "El poder oculto en las sombras 🎄",
+          body: "El poder oculto en las sombras",
           thumbnailUrl: 'https://files.catbox.moe/owpjte.jpg',
           sourceUrl: json.html_url, 
           mediaType: 1,
@@ -33,7 +34,7 @@ let handler = async (m, { conn }) => {
     }, { quoted: m })
 
   } catch {
-    await conn.reply(m.chat, `🌑🎄 ¡Gomen! Ocurrió un error al acceder al dominio de las sombras. ❄️`, m)
+    await conn.reply(m.chat, `🌑⚔️ ¡Gomen! Ocurrió un error al acceder al dominio de las sombras.`, m)
   }
 }
 
