7ddfc460bca1c313af98d909bcdc15549c50692f
yosue891 <yosueortega630@gmail.com>
2026-01-07 22:00:39 -0400

    Update dueño-fix.js

--- plugins/dueño-fix.js
+++ plugins/dueño-fix.js
@@ -8,9 +8,8 @@ const stdout = execSync('git pull' + (m.fromMe && text ? ' ' + text : ''));
 let messager = stdout.toString()
 if (messager.includes('❀ Ya está cargada la actualización.')) messager = '❀ Los datos ya están actualizados a la última versión.'
 if (messager.includes('ꕥ Actualizando.')) messager = '❀ Procesando, espere un momento mientras me actualizo.\n\n' + stdout.toString()
-messager = `「✦」Actualización realizada.\n> ✐ Resultado »\n${messager}`
 await m.react('✔️')
-conn.reply(m.chat, messager, m)
+conn.reply(m.chat, `「✦」Actualización realizada.\n> ✐ Resultado »\n${stdout.toString()}`, m)
 } catch { 
 try {
 const status = execSync('git status --porcelain')
@@ -21,7 +20,7 @@ return null
 }
 return '→ ' + line.slice(3) + ''}).filter(Boolean)
 if (conflictedFiles.length > 0) {
-const errorMessage = `⚠︎ No se pudo realizar la actualización:\n\n> Se han encontrado cambios locales en los archivos del bot que entran en conflicto con las nuevas actualizaciones del repositorio.\n\n${conflictedFiles.join('\n')}.`
+const errorMessage = \⚠︎ No se pudo realizar la actualización:\\n\n> Se han encontrado cambios locales en los archivos del bot que entran en conflicto con las nuevas actualizaciones del repositorio.\n\n${conflictedFiles.join('\n')}.
 await conn.reply(m.chat, errorMessage, m)
 await m.react('✖️')
 }}} catch (error) {
