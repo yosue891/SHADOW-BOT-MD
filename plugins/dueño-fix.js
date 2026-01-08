6e686e286dbf36c6b461a054c67ed1ae1f438f40
yosue891 <yosueortega630@gmail.com>
2026-01-07 22:25:32 -0400

    Update dueño-fix.js

--- plugins/dueño-fix.js
+++ plugins/dueño-fix.js
@@ -8,9 +8,8 @@ const stdout = execSync('git pull' + (m.fromMe && text ? ' ' + text : ''));
 let messager = stdout.toString()
 if (messager.includes('❀ Ya está cargada la actualización.')) messager = '❀ Los datos ya están actualizados a la última versión.'
 if (messager.includes('ꕥ Actualizando.')) messager = '❀ Procesando, espere un momento mientras me actualizo.\n\n' + stdout.toString()
-messager = `「✦」Actualización realizada.\n> ✐ Resultado »\n${messager}`
 await m.react('✔️')
-conn.reply(m.chat, messager, m)
+conn.reply(m.chat, messager, m, { ...rcanal })
 } catch { 
 try {
 const status = execSync('git status --porcelain')
@@ -19,10 +18,10 @@ const conflictedFiles = status.toString().split('\n').filter(line => line.trim()
 if (line.includes('.npm/') || line.includes('.cache/') || line.includes('tmp/') || line.includes('database.json') || line.includes('sessions/Principal/') || line.includes('npm-debug.log')) {
 return null
 }
-return '→ ' + line.slice(3) + ''}).filter(Boolean)
+return '*→ ' + line.slice(3) + '*'}).filter(Boolean)
 if (conflictedFiles.length > 0) {
-const errorMessage = `⚠︎ No se pudo realizar la actualización:\n\n> Se han encontrado cambios locales en los archivos del bot que entran en conflicto con las nuevas actualizaciones del repositorio.\n\n${conflictedFiles.join('\n')}.`
-await conn.reply(m.chat, errorMessage, m)
+const errorMessage = `\`⚠︎ No se pudo realizar la actualización:\`\n\n> *Se han encontrado cambios locales en los archivos del bot que entran en conflicto con las nuevas actualizaciones del repositorio.*\n\n${conflictedFiles.join('\n')}.`
+await conn.reply(m.chat, errorMessage, m, { ...rcanal })
 await m.react('✖️')
 }}} catch (error) {
 console.error(error)
@@ -30,7 +29,7 @@ let errorMessage2 = '⚠︎ Ocurrió un error inesperado.'
 if (error.message) {
 errorMessage2 += '\n⚠︎ Mensaje de error: ' + error.message
 }
-await conn.reply(m.chat, errorMessage2, m)
+await conn.reply(m.chat, errorMessage2, m, { ...rcanal })
 }}}
 
 handler.help = ['update']
