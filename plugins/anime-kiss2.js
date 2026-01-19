27375a24a7ddca770b491f616d13ae0086b1a653
yosue891 <yosueortega630@gmail.com>
2026-01-19 18:44:47 -0400

    Update anime-kiss2.js

--- plugins/anime-kiss2.js
+++ plugins/anime-kiss2.js
@@ -26,30 +26,13 @@ let handler = async (m, { conn, usedPrefix }) => {
     }
     
     if (m.isGroup) {
-        let pp = 'https://qu.ax/bLLe.mp4'; 
-        let pp2 = 'https://qu.ax/mwXW.mp4'; 
-        let pp3 = 'https://qu.ax/WUiG.mp4';
-        let pp4 = 'https://qu.ax/djk.mp4';
-        let pp5 = 'https://qu.ax/xdis.mp4';
-        let pp6 = 'https://qu.ax/JKEw.mp4';
-        let pp7 = 'https://qu.ax/eCmG.mp4';
-        let pp8 = 'https://qu.ax/Rtaw.mp4';
-        let pp9 = 'https://qu.ax/Esky.mp4';
-        let pp10 = 'https://qu.ax/AYoP.mp4';
-        let pp11 = 'https://qu.ax/ulK.mp4';
-        let pp12 = 'https://qu.ax/xgVd.mp4';
-        let pp13 = 'https://qu.ax/gchC.mp4';
-        let pp14 = 'https://qu.ax/DSbr.mp4';
-        let pp15 = 'https://qu.ax/duCR.mp4';
-        let pp16 = 'https://qu.ax/aHmt.mp4';
-        let pp17 = 'https://qu.ax/BmUb.mp4';
-        let pp18 = 'https://qu.ax/lBqZ.mp4';
-        let pp19 = 'https://qu.ax/LcxW.mp4';
-        let pp20 = 'https://qu.ax/MacM.mp4';
-        let pp21 = 'https://qu.ax/vwbX.mp4';
-        let pp22 = 'https://qu.ax/hnzN.mp4';
-        let pp23 = 'https://qu.ax/hqZa.mp4';
-        let pp24 = 'https://qu.ax/WUE.mp4';
+        let pp = 'https://files.catbox.moe/0p0gsn.mp4'; 
+        let pp2 = 'https://files.catbox.moe/me6rsr.mp4'; 
+        let pp3 = 'https://files.catbox.moe/untes1.mp4';
+        let pp4 = 'https://files.catbox.moe/8af0gd.mp4';
+        let pp5 = 'https://files.catbox.moe/z27nnd.mp4';
+        let pp6 = 'https://files.catbox.moe/c5fxap.mp4';
+        let pp7 = 'https://files.catbox.moe/2c3ejd.mp4';
                 
         const videos = [pp, pp2, pp3, pp4, pp5, pp6, pp7, pp8, pp9, pp10, pp11, pp12, pp13, pp14, pp15, pp16, pp17, pp18, pp19, pp20, pp21, pp22, pp23, pp24];
         const video = videos[Math.floor(Math.random() * videos.length)];
