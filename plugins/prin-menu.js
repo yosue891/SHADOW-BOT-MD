03c3eacc5e4e01e2e77613c80c73cb5fe3bfe2e1
yosue891 <yosueortega630@gmail.com>
2026-02-13 18:09:32 -0400

    Update prin-menu.js

--- plugins/prin-menu.js
+++ plugins/prin-menu.js
@@ -31,7 +31,7 @@ let handler = async (m, { conn, usedPrefix }) => {
 
     let user = global.db.data.users[m.sender]
     let nombre = await conn.getName(m.sender)
-    let premium = user?.premium ? '✅ Sí' : '❌ No'
+    let registrado = user?.registered ? '✅ Sí' : '❌ No'
     let limite = user?.limit || 0
     let totalreg = Object.keys(global.db.data.users).length
     let groupsCount = Object.values(conn.chats).filter(v => v.id.endsWith('@g.us')).length
@@ -49,8 +49,8 @@ let handler = async (m, { conn, usedPrefix }) => {
 ꜱᴏʏ 🪴 ღSHADOW-BOT-MD༻๖ۣۜ◥ὦɧ◤🪴, ʟɪꜱᴛᴏ ᴘᴀʀᴀ ᴀʏᴜᴅᴀʀᴛᴇ
 
 乂 ɪɴꜰᴏ ᴅᴇʟ ᴜꜱᴜᴀʀɪᴏ
-┌  ◦ ᴇꜱᴛᴀᴅᴏ: ᴜꜱᴜᴀʀɪᴏ
-│  ◦ ᴘʀᴇᴍɪᴜᴍ: ${premium}
+┌  ◦ ᴇꜱᴛᴀᴅᴏ: ${user?.registered ? 'Registrado' : 'No Registrado'}
+│  ◦ ʀᴇɢɪꜱᴛʀᴀᴅᴏ: ${registrado}
 └  ◦ ʟíᴍɪᴛᴇ: ${limite}
 
 乂 ɪɴꜰᴏ ᴅᴇʟ ʙᴏᴛ
@@ -82,7 +82,6 @@ let handler = async (m, { conn, usedPrefix }) => {
     let finalMenu = infoUser + '\n\n' + menu.join('\n\n') + '\n' + after
     let imagen = 'https://files.catbox.moe/h8lydl.jpg'
 
-
     let media = await prepareWAMessageMedia(
         { image: { url: imagen } },
         { upload: conn.waUploadToServer }
@@ -96,7 +95,7 @@ let handler = async (m, { conn, usedPrefix }) => {
                         hasMediaAttachment: true,
                         imageMessage: media.imageMessage 
                     },
-                    body: { text: finalMenu }, // El menú generado va aquí
+                    body: { text: finalMenu },
                     footer: { text: "🪴 .ღSHADOW-BOT-MD༻๖ۣۜ◥ὦɧ◤🪴" },
                     nativeFlowMessage: {
                         buttons: [
@@ -133,51 +132,8 @@ let handler = async (m, { conn, usedPrefix }) => {
                                     url: "https://whatsapp.com/channel/0029VbArz9fAO7RGy2915k3O",
                                     merchant_url: "https://whatsapp.com/channel/0029VbArz9fAO7RGy2915k3O"
                                 })
-                            },
-                            {
-                                name: "galaxy_message",
-                                buttonParamsJson: JSON.stringify({
-                                    mode: "published",
-                                    flow_message_version: "3",
-                                    flow_token: "1:1307913409923914:293680f87029f5a13d1ec5e35e718af3",
-                                    flow_id: "1307913409923914",
-                                    flow_cta: "ACCEDE A BOT AI",
-                                    flow_action: "navigate",
-                                    flow_action_payload: {
-                                        screen: "QUESTION_ONE",
-                                        params: { user_id: "123456789", referral: "campaign_xyz" }
-                                    },
-                                    flow_metadata: {
-                                        flow_json_version: "201",
-                                        data_api_protocol: "v2",
-                                        flow_name: "Lead Qualification [en]",
-                                        data_api_version: "v2",
-                                        categories: ["Lead Generation", "Sales"]
-                                    }
-                                })
                             }
-                        ],
-                        messageParamsJson: JSON.stringify({
-                            limited_time_offer: {
-                                text: "꩜ 𝗠𝗲𝗻𝘂 𝗟𝗶𝘀𝘁",
-                                url: "https://whatsapp.com/channel/0029VbArz9fAO7RGy2915k3O",
-                                copy_code: "SHADOW-BOT-MD",
-                                expiration_time: 1754613436864329
-                            },
-                            bottom_sheet: {
-                                in_thread_buttons_limit: 2,
-                                divider_indices: [1, 2, 3],
-                                list_title: "Select Menu",
-                                button_title: "⊱✿ ᴍᴇɴᴜ ʟɪsᴛ ✿⊰"
-                            },
-                            tap_target_configuration: {
-                                title: "▸ SHADOW ◂",
-                                description: "Menú Principal",
-                                canonical_url: "https://whatsapp.com/channel/0029VbArz9fAO7RGy2915k3O",
-                                domain: "https://whatsapp.com",
-                                button_index: 0
-                            }
-                        })
+                        ]
                     },
                     contextInfo: {
                         mentionedJid: [m.sender],
