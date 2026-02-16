4236c8233ea9eae8c9090fe63415fce5bc845187
yosue891 <yosueortega630@gmail.com>
2026-02-16 15:29:00 -0400

    Update fun-afk2.js

--- plugins/fun-afk2.js
+++ plugins/fun-afk2.js
@@ -1,52 +1,14 @@
-import fetch from 'node-fetch'
-
 const handler = async (m, { conn, text }) => {
   const user = global.db.data.users[m.sender]
 
+  // Marca al usuario como AFK
   user.afk = +new Date
   user.afkReason = text || ''
 
-  let thumb = null
-  try {
-    thumb = await (await fetch("https://i.postimg.cc/rFfVL8Ps/image.jpg")).buffer()
-  } catch {
-    thumb = null
-  }
-
-  const shadow_xyz = {
-    key: {
-      remoteJid: "status@broadcast",
-      fromMe: false,
-      id: "ShadowCatalog",
-      participant: "0@s.whatsapp.net"
-    },
-    message: {
-      productMessage: {
-        product: {
-          productImage: thumb
-            ? {
-                mimetype: "image/jpeg",
-                jpegThumbnail: thumb
-              }
-            : undefined,
-          title: "WhatsApp Business • Estado",
-          description: "Shadow team",
-          currencyCode: "USD",
-          priceAmount1000: 0000,
-          retailerId: "ShadowCore",
-          productImageCount: 1
-        },
-        businessOwnerJid: "584242773183@s.whatsapp.net"
-      }
-    }
-  }
-
-  await conn.sendMessage(
+  await conn.reply(
     m.chat,
-    {
-      text: `🌌 *Discípulo de las Sombras* 🎄\nHas entrado en estado AFK.\n○ Motivo » *${text || 'sin especificar'}*`
-    },
-    { quoted: shadow_xyz }
+    `🌌 *Discípulo de las Sombras* 🎄\nHas entrado en estado AFK.\n○ Motivo » *${text || 'sin especificar'}*`,
+    m
   )
 }
 
