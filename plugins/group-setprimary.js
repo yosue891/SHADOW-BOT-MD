18a0e7478e2c0def21f8d1d40dd2f931889a1ddc
The King wilker <sandovalwilkerson700@gmail.com>
2026-06-10 12:50:33 -0400

    Actualizar group-setprimary.js

--- plugins/group-setprimary.js
+++ plugins/group-setprimary.js
@@ -1,57 +1,29 @@
 import ws from 'ws'
 
-const handler = async (m, { conn, usedPrefix }) => {
-  const subBots = [
-    ...new Set(
-      [
-        ...global.conns
-          .filter((conn) => conn.user && conn.ws.socket && conn.ws.socket.readyState !== ws.CLOSED)
-          .map((conn) => conn.user.jid),
-      ]
-    ),
-  ]
+const handler = async (m, { conn }) => {
+  const subBots = [...new Set([...global.conns.filter((conn) => conn.user && conn.ws.socket && conn.ws.socket.readyState !== ws.CLOSED).map((conn) => conn.user.jid)])]
 
   if (global.conn?.user?.jid && !subBots.includes(global.conn.user.jid)) {
     subBots.push(global.conn.user.jid)
   }
 
-  const chat = global.db.data.chats?.[m.chat] || {}
-  const mentionedJid = m.mentionedJid
-  const who = mentionedJid && mentionedJid[0] ? mentionedJid[0] : m.quoted ? m.quoted.sender : false
-  
-  if (!who) return conn.reply(m.chat, `❀ Por favor, menciona a un Socket para hacerlo Bot principal del grupo.`, m)
-  if (!subBots.includes(who)) return conn.reply(m.chat, `ꕥ El usuario mencionado no es un Socket de: *${global.botname}*.`, m)
+  const chat = global.db.data.chats[m.chat]
+  const mentionedJid = await m.mentionedJid
+  const who = mentionedJid[0] ? mentionedJid[0] : m.quoted ? await m.quoted.sender : false
+
+  if (!who) return conn.reply(m.chat, `❀ *Por favor, menciona al Saiyan (o Sub-Saiyan) o responde a un mensaje suyo para seleccionarlo como dios supremo.*`, m)
+
+  if (!subBots.includes(who)) return conn.reply(m.chat, `ꕥ *El usuario indicado no es un Bot activo del sistema.*`, m)
+
   if (chat.primaryBot === who) {
-    return conn.reply(
-      m.chat,
-      `⌗ @${who.split`@`[0]} ya está como Bot primario en este grupo.`,
-      m,
-      { mentions: [who] }
-    )
+    return conn.reply(m.chat, `➜ @${who.split`@`[0]} *ya está configurado como el Bot Principal aquí.*`, m, { mentions: [who] });
   }
 
   try {
-    if (!global.db.data.chats[m.chat]) {
-        global.db.data.chats[m.chat] = { primaryBot: who }
-    } else {
-        global.db.data.chats[m.chat].primaryBot = who
-    }
-    
-    await global.db.write()
-    
-    conn.reply(
-      m.chat,
-      `✏ Se ha establecido a @${who.split`@`[0]} como Bot primario de este grupo.\n> Ahora todos los comandos de este grupo serán ejecutados por @${who.split`@`[0]}.`,
-      m,
-      { mentions: [who] }
-    )
+    chat.primaryBot = who
+    conn.reply(m.chat, `✰ *¡CONFIGURACIÓN ACTUALIZADA!* ✰\n\n➜ *Nuevo Shadow supremo:* @${who.split`@`[0]}\n> A partir de ahora, los comandos en este grupo serán ejecutados preferentemente por este * Shadow bot*.`, m, { mentions: [who] })
   } catch (e) {
-    console.error(e)
-    conn.reply(
-      m.chat,
-      `⚠︎ Se ha producido un problema.\n> Usa *${usedPrefix}report* para informarlo.\n\n${e.message}`,
-      m
-    )
+    conn.reply(m.chat, `⚠︎ *Ocurrió un error inesperado:*\n> ${e.message}`, m)
   }
 }
 
