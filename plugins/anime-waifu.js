db4bff96023a6af16989984e082edbbb9d1b10bc
yosue891 <yosueortega630@gmail.com>
2026-06-25 15:38:54 -0400

    Update anime-waifu.js

--- plugins/anime-waifu.js
+++ plugins/anime-waifu.js
@@ -33,7 +33,13 @@ let handler = async (m, { conn, usedPrefix, command }) => {
     await m.react('❤️');
     await conn.reply(m.chat, '🌌 *Buscando una waifu en alta definición...*', m, { contextInfo });
 
-    const res = await axios.get('https://api.waifu.im/search?is_nsfw=false');
+    const res = await axios.get('https://api.waifu.im/search?is_nsfw=false', {
+      headers: {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
+        'Accept': 'application/json',
+        'Accept-Language': 'en-US,en;q=0.9'
+      }
+    });
     
     if (!res.data?.images?.[0]?.url) throw new Error('No se pudo obtener la waifu.');
 
