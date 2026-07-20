8cca84fe5244cc834fa561f4d82b5dba6ed3011f
yosue891 <yosueortega630@gmail.com>
2026-07-20 13:25:26 -0400

    Update prin-menugacha.js

--- plugins/prin-menugacha.js
+++ plugins/prin-menugacha.js
@@ -58,7 +58,7 @@ let handler = async (m, { conn, usedPrefix }) => {
 *╰─────────────╯*`.trim()
 
     let finalMenu = infoUser + '\n\n' + comandosGacha
-    let imagen = 'https://d.uguu.se/NYxEwzWD.jpeg'
+    let imagen = 'https://d.uguu.se/NYxEwzWd.jpeg'
 
     let vcard = `BEGIN:VCARD\nVERSION:3.0\nN:;Itachi;;;\nFN:Itachi\nitem1.TEL;waid=13135550002:+1 (313) 555-0002\nitem1.X-ABLabel:Celular\nEND:VCARD`
     let qkontak = { 
