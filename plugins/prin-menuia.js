b9edb168401d982205fb95395b37ef2d883d8236
yosue891 <yosueortega630@gmail.com>
2026-07-20 13:33:59 -0400

    Update image URL in prin-menuia.js

--- plugins/prin-menuia.js
+++ plugins/prin-menuia.js
@@ -39,7 +39,7 @@ let handler = async (m, { conn, usedPrefix }) => {
 *╰─────────────╯*`.trim()
 
     let finalMenu = infoUser + '\n\n' + comandosAi
-    let imagen = 'https://h.uguu.se/zAqcgozY.jpeg'
+    let imagen = 'https://n.uguu.se/LltydEaL.jpeg'
 
     let vcard = `BEGIN:VCARD\nVERSION:3.0\nN:;Itachi;;;\nFN:Itachi\nitem1.TEL;waid=13135550002:+1 (313) 555-0002\nitem1.X-ABLabel:Celular\nEND:VCARD`
     let qkontak = { 
