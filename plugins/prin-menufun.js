4fa521aea0752cc359c7394996cf04db8c928f51
yosue891 <yosueortega630@gmail.com>
2026-07-20 13:29:52 -0400

    Update prin-menufun.js

--- plugins/prin-menufun.js
+++ plugins/prin-menufun.js
@@ -61,7 +61,7 @@ let handler = async (m, { conn, usedPrefix }) => {
 *╰─────────────╯*`.trim()
 
     let finalMenu = infoUser + '\n\n' + comandosOwner
-    let videoUrl = 'https://i.ibb.co/3NfYh9k/default-avatar.png'
+    let videoUrl = 'https://d.uguu.se/PXjpyfPU.jpeg'
 
     let vcard = `BEGIN:VCARD\nVERSION:3.0\nN:;Itachi;;;\nFN:Itachi\nitem1.TEL;waid=13135550002:+1 (313) 555-0002\nitem1.X-ABLabel:Celular\nEND:VCARD`
     let qkontak = { 
