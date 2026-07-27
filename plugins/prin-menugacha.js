58d6c99986271a80c2f6cc7d5a28449eb6b264f9
yosue891 <yosueortega630@gmail.com>
2026-07-27 10:24:48 -0400

    Update image URL in prin-menugacha.js

--- plugins/prin-menugacha.js
+++ plugins/prin-menugacha.js
@@ -58,7 +58,7 @@ let handler = async (m, { conn, usedPrefix }) => {
 *╰─────────────╯*`.trim()
 
     let finalMenu = infoUser + '\n\n' + comandosGacha
-    let imagen = 'https://d.uguu.se/NYxEwzWd.jpeg'
+    let imagen = 'https://raw.githubusercontent.com/yosue891/Shadow-MD/refs/heads/main/Gemini_Generated_Image_kil16gkil16gkil1.png'
 
     let vcard = `BEGIN:VCARD\nVERSION:3.0\nN:;Itachi;;;\nFN:Itachi\nitem1.TEL;waid=13135550002:+1 (313) 555-0002\nitem1.X-ABLabel:Celular\nEND:VCARD`
     let qkontak = { 
