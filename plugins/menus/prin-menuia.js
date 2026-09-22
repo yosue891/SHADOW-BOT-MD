aa9beb786a59825d5cf5b9704fd59452ed90c30f
Leo <euclidescampos136@gmail.com>
2026-09-22 15:11:43 -0400

    Actualizar prin-menuia.js

--- plugins/menus/prin-menuia.js
+++ plugins/menus/prin-menuia.js
@@ -39,7 +39,7 @@ let handler = async (m, { conn, usedPrefix }) => {
 *╰─────────────╯*`.trim()
 
     let finalMenu = infoUser + '\n\n' + comandosAi
-    let imagen = 'https://raw.githubusercontent.com/yosue891/Shadow-MD/refs/heads/main/Gemini_Generated_Image_5aj2sm5aj2sm5aj2.png'
+    let imagen = 'https://github.com/Dv-Leo/imagenes/blob/main/Shadow%2FIMG-20260921-WA0006.jpg?raw=true'
 
     let vcard = `BEGIN:VCARD\nVERSION:3.0\nN:;Itachi;;;\nFN:Itachi\nitem1.TEL;waid=13135550002:+1 (313) 555-0002\nitem1.X-ABLabel:Celular\nEND:VCARD`
     let qkontak = { 
